abf0410cfe6bb71c39d797a30893e034001d33d0
SJM <warning6715@gmail.com>
2023-03-23 15:13:07 +0900

    form연습

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,12 +1,23 @@
 import classes from './test.module.scss'
+import React, {useState} from 'react'
 
-function InputText(){
+export function InputText(props){
+  const[email, setEmail]=useState("");
+  const[pw,setPw]=useState("")
 
   return(
     <div>
-      <input type="text" className={classes.text}/>
+      <input type="text" className={classes.text} placeholder={props.detail} value={email} onChange={(e)=>setEmail(e.target.value)} />
     </div>
   );
 }
 
-export default InputText;
+export  function IDVal(props){
+  const[ID,setID]=useState("");
+
+  return(
+    <div>
+      <input type="text"/>
+    </div>
+  )
+}
---
+++ src/pages/form.jsx
@@ -0,0 +1,10 @@
+import {InputText, IDVal} from "@/components/test";
+
+export default function Form(){
+  return(
+    <>
+    <InputText detail="ID를 입력해주세요" />
+    <IDVal />
+    </>
+  );
+}
